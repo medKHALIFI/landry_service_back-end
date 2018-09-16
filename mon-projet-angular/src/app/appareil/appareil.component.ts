f90688e7592326da0e412bc605d4a393c0862fab
medkhalifi <med.khalifi95@outlook.fr>
2018-09-16 10:56:20 +0100

    ajouter les status des machines

--- mon-projet-angular/src/app/appareil/appareil.component.ts
+++ mon-projet-angular/src/app/appareil/appareil.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppareilComponent implements OnInit {
 
   appareilName: string = 'Machine à laver';
+  appareilStatus: string = 'éteint';
 
   constructor() { }
 
