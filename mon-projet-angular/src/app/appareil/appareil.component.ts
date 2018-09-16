12f71b6440ad2c34bc5a22b4927cb3d9bf466025
medkhalifi <med.khalifi95@outlook.fr>
2018-09-16 10:48:21 +0100

    les noms des machines

--- mon-projet-angular/src/app/appareil/appareil.component.ts
+++ mon-projet-angular/src/app/appareil/appareil.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppareilComponent implements OnInit {
 
+  appareilName: string = 'Machine à laver';
+
   constructor() { }
 
   ngOnInit() {
